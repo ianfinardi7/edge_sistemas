5112584cfec78ce83aff0cbeb778af5f03614506
Ian <azuligamer5@gmail.com>
2026-08-19 01:45:19 -0300

    Reestruturação de pastas e arquivos do projeto

---
+++ src/main/java/br/com/ianfinardi/ProjetoEdgeSistemasApplication.java
@@ -0,0 +1,12 @@
+package br.com.ianfinardi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEdgeSistemasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoEdgeSistemasApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/ianfinardi/controller/ProdutoController.java
@@ -0,0 +1,73 @@
+package br.com.ianfinardi.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.com.ianfinardi.model.Produto;
+import br.com.ianfinardi.repository.ProdutoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listar() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produtos/{codigo}")
+	public ResponseEntity<Produto> buscarPorCodigo(@PathVariable int codigo) {
+		Optional<Produto> produto = produtoRepository.findById(codigo);
+		if (produto.isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).body(produto.get());
+			
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	
+	@PostMapping("/produtos")
+	public ResponseEntity<Produto> cadastrar(@Valid @RequestBody Produto produto) {
+		try {
+			if (produtoRepository.existsById(produto.getCodigo())) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		Produto salvo = produtoRepository.save(produto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
+	} catch (Exception e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
+
+	@PutMapping("/produtos/{codigo}")
+	public ResponseEntity<Produto> atualizar(@Valid @PathVariable int codigo, @RequestBody Produto produto) {
+		if (!produtoRepository.existsById(codigo)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		produto.setCodigo(codigo);
+		Produto atualizado = produtoRepository.save(produto);
+		return ResponseEntity.status(HttpStatus.OK).body(atualizado);
+	}
+
+
+	@DeleteMapping("/produto/{codigo}")
+	public ResponseEntity<Void> excluir(@PathVariable int codigo) {
+		if (!produtoRepository.existsById(codigo)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+			
+		}
+		produtoRepository.deleteById(codigo);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+}
